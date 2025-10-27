e3f18db8a901f4cc09b7e0f9565f19fed89e19a3
Ignacio Blanquero <ignacio.blanquero@gmail.com>
2025-10-27 11:13:27 +0100

    feat: writing, create, read & delete views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
 
@@ -10,3 +11,25 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() #Define el conjunto de objetos de base de datos (QuerySet) que esta vista está destinada a manejar.
     serializer_class = UserSerializer #Especifica qué clase de serializador debe usar la vista para procesar los datos entrantes.
     permission_classes = [AllowAny] #Controla quién puede acceder a esta vista.
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
