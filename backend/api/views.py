c16ec5c42cf65b2f1b3539acad4c8b73c965807e
Ignacio Blanquero <ignacio.blanquero@gmail.com>
2025-10-27 10:25:15 +0100

    feat: registration view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() #Define el conjunto de objetos de base de datos (QuerySet) que esta vista está destinada a manejar.
+    serializer_class = UserSerializer #Especifica qué clase de serializador debe usar la vista para procesar los datos entrantes.
+    permission_classes = [AllowAny] #Controla quién puede acceder a esta vista.
